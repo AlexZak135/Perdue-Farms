8a20e9cc782df36052f36604a1c35900a36b6cfd
AlexZak135 <azakrzeski13@gmail.com>
2025-06-16 22:24:34 -0400

    Update

--- Code/Perdue-Farms-Code.py
+++ Code/Perdue-Farms-Code.py
@@ -1,6 +1,6 @@
 # Title: Perdue Farms Analysis
 # Author: Alexander Zakrzeski
-# Date: June 15, 2025
+# Date: June 16, 2025
 
 # Part 1: Setup and Configuration
 
@@ -20,10 +20,28 @@ tms, dc, otht = (
                   "On-Time-Held-Time-Data.parquet"]]
     )
 
-tms = tms.with_columns(pl.col("shipment_number").cast(pl.Utf8))
-tms = tms.filter(pl.col("carrier_name") == "Perdue")
-tms = tms.drop("carrier_name")
+tms = (
+    tms.rename({"driver_#": "driver_number"})
+       .with_columns(
+           *[pl.col(c).cast(pl.Utf8).str.zfill(6) if c == "driver_number" 
+             else pl.col(c).cast(pl.Utf8) 
+             for c in ["shipment_number", "driver_number"]],
+           *[pl.col(c).str.replace(r"^St ", "St. ")
+             for c in ["pickup_city", "dropoff_city"]]
+      ).with_columns(
+           pl.col("dropoff_city").str.replace_many(
+               ["LINDEN", "mankato", "N Dartmouth", "S Portland", 
+                "Warrensville Ht"],
+               ["Linden", "Mankato", "North Dartmouth", "South Portland", 
+                "Warrensville Heights"]
+               ),
+           (pl.col("pickup_depart_date") + " " + 
+            pl.col("pickup_depart_time")).str.strptime(pl.Datetime, 
+                                                       "%Y-%m-%d %H:%M:%S")
+              .alias("pickup_timestamp") 
+      ).filter(pl.col("carrier_name") == "Perdue")
+       .drop("carrier_name", "pickup_id", "pickup_depart_date", 
+             "pickup_depart_time")
+    )
 
-
-
-check = tms.select(pl.col("carrier_name").value_counts(sort = True))
+print(list(zip(tms.columns, tms.dtypes)))
